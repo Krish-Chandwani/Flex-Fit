3147c50567420c821e6e6917f49b1d0338e6861a
Krish-Chandwani <krishc113@gmail.com>
2026-03-01 21:42:22 +0530

    Made Navbar Responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../Assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const [visible,setVisible] = useState(false)
+
   return (
     <div className='flex items-center justify-between py-5 font-medium '>
       <img className='w-36'src={assets.logo} alt="logo" />
-      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'></ul>
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
       <NavLink className='flex flex-col items-center gap-1' to='/'>
         <p>HOME</p>
@@ -25,6 +27,8 @@ const Navbar = () => {
         <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
       </NavLink>
 
+      </ul>
+
       <div className='flex items-center gap-6'>
         <img src={assets.search_icon} className='w-5 cursor-pointer'alt="search" />
         <div className='group relative'>
@@ -39,8 +43,25 @@ const Navbar = () => {
         </div>
         <Link to='/cart' className='relative'>
             <img src={assets.cart_icon} className='w-5 min-w-5'alt="carticon" />
-            <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
+            <p className='absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>0</p>
         </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="menuicon" />
+
+      </div>
+
+      {/* sidebar menu for small screens */}
+      <div className={`absolute top-0 bottom-0 right-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=>setVisible(false)}className='flex items-center gap-4 p-3 cursor-pointer'>
+            <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="dropdown" />
+            <p>Back</p>
+          </div>
+
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'> Home </NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'> Collection </NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'> About </NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'> Contact </NavLink>
+        </div>
 
       </div>
     </div>
